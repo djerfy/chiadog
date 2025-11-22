55090b715056872ab07140c552e98fa6fb1350c9
djerfy <djerfy@gmail.com>
2025-11-22 12:08:26 +0100

    feat: support of Chia v2.5.7 (issue #2)
    Signed-off-by: djerfy <djerfy@gmail.com>

--- tests/chia_log/parsers/test_harvester_activity_parser.py
+++ tests/chia_log/parsers/test_harvester_activity_parser.py
@@ -12,31 +12,32 @@ class TestHarvesterActivityParser(unittest.TestCase):
         self.example_logs_path = Path(__file__).resolve().parents[1] / "logs/harvester_activity"
         with open(self.example_logs_path / "nominal.txt", encoding="UTF-8") as f:
             self.nominal_logs = f.read()
-        with open(self.example_logs_path / "nominal_old_log_format.txt", encoding="UTF-8") as f:
-            self.nominal_logs_old_format = f.read()
 
     def tearDown(self) -> None:
         pass
 
     def testBasicParsing(self):
-        for logs in [self.nominal_logs, self.nominal_logs_old_format]:
+        for logs in [self.nominal_logs]:
             # Check that important fields are correctly parsed
             activity_messages = self.parser.parse(logs)
             self.assertNotEqual(len(activity_messages), 0, "No log messages found")
 
-            expected_eligible_plot_counts = [0, 1, 2, 3, 0]
-            expected_proofs_found_counts = [0, 0, 1, 0, 0]
+            expected_eligible_plot_counts = [8, 3, 3, 5, 3]
+            expected_proofs_found_counts = [0, 1, 1, 2, 1]
+            expected_qualities_found_counts = [0, 0, 0, 0, 1]
             expected_search_times = [0.55515, 1.05515, 0.23412, 0.12348, 0.34952]
             expected_total_plots_counts = [42, 42, 42, 43, 43]
-            for msg, eligible, found, search, total in zip(
+            for msg, eligible, proofs, qualities, search, total in zip(
                 activity_messages,
                 expected_eligible_plot_counts,
                 expected_proofs_found_counts,
+                expected_qualities_found_counts,
                 expected_search_times,
                 expected_total_plots_counts,
             ):
                 self.assertEqual(msg.eligible_plots_count, eligible, "Eligible plots count don't match")
-                self.assertEqual(msg.found_proofs_count, found, "Found proofs count don't match")
+                self.assertEqual(msg.found_proofs_count, proofs, "Found proofs count don't match")
+                self.assertEqual(msg.found_qualities_count, qualities, "Found qualities count don't match")
                 self.assertEqual(msg.search_time_seconds, search, "Search time seconds don't match")
                 self.assertEqual(msg.total_plots_count, total, "Total plots count don't match")
 
